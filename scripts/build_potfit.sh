d3411cb02b8ddcfea2ef058521b71ac9bc8add12
Naseer Dari <naseerdari01@gmail.com>
2017-12-02 13:30:24 -0600

    Add the build_potfit script

---
+++ scripts/build_potfit.sh
@@ -0,0 +1,26 @@
+#/usr/bin/env bash
+
+set -eux
+
+BUILD_DIR="_build"
+POTFIT_REPO="https://github.com/potfit/potfit.git"
+POTFIT_RELEASE="0.7.1"
+POT_OPTION="potfit_mpi_tersoff_apot"
+
+if [ -d $BUILD_DIR ]; then
+  rm -rf $BUILD_DIR
+fi
+
+CURR_DIR=`pwd`
+
+git clone $POTFIT_REPO $BUILD_DIR
+cd $BUILD_DIR
+git checkout tags/$POTFIT_RELEASE
+
+sed -i .bak -e 's/mpicc/mpiicc/' -e 's/^BIN_DIR/#&/' Makefile
+
+module load intel
+
+make $POT_OPTION
+cp $POT_OPTION ../
+
